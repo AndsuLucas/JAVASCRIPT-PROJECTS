1295aec19f1c0231a91f496aad4b96d9863ba6b7
Anderson Lucas <a.lucassilvadeoliveira@gmail.com>
2019-07-19 18:50:50 -0300

    mudanças pequenas

--- jogo_memoria_v2/assets/scripts/time.js
+++ jogo_memoria_v2/assets/scripts/time.js
@@ -9,7 +9,8 @@
 	tempo.setHours(0);
 	tempo.setMinutes(0);
 	tempo.setSeconds(0);
-	console.log(domMinuto);
+	
+
 	domSegundo.textContent = tempo.getSeconds();
 	domHora.textContent    = tempo.getHours();
 	domMinuto.textContent  = tempo.getMinutes();
@@ -22,7 +23,8 @@
 		segundo ++;
 		//setando o segundo do objeto
 		tempo.setSeconds(segundo);
-		console.log(segundo);
+	
+	
 		domSegundo.textContent = tempo.getSeconds();
 		domHora.textContent    = tempo.getHours()
 		domMinuto.textContent  = tempo.getMinutes()
